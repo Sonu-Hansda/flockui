4ea75940716311ff3fe486ff81db3dfb991390f7
Sonu-Hansda <sonukumarhansda61@gmail.com>
2026-05-24 22:23:51 +0530

    add ko-fi domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.ko-fi.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
